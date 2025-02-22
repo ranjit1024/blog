593e64816149f0dba8aec996dd0cf89bb80a501a
ranjit1024 <ranjitdas2048@gmail.com>
2025-02-22 07:14:08 +0530

    chnged temp

--- FrontEnd/src/components/blog.tsx
+++ FrontEnd/src/components/blog.tsx
@@ -1,20 +1,20 @@
 export function BlogingComp() {
-    return <div className="w-[80%]  font-inter font-normal text-[1.3rem] p-5 bg-green-600 bg-opacity-90 rounded-lg  shadow-[0_3px_10px_rgb(0,0,0,0.2)] "
+    return <div className="w-[80%]  font-inter font-normal text-[1.3rem] p-5 bg- bg-slate-50 bg-opacity-90 rounded-lg  shadow-[0_3px_10px_rgb(0,0,0,0.2)] "
     >
         <p className="text-blue-800 text-[1.5rem] font-[500]">#1</p>
-        <p className="font-[500] text-gray-100">
+        <p className="font-[500] text-gray-900">
             “Your first blog posts won’t be perfect, but you just have to do it. You have to start somewhere.”  <span className="font-[600] text-slate-900">– Shane Barker</span>
         </p>
     </div>
 }
 
 export function BlogingComp2() {
-    return <div className="w-[80%]   font-inter font-normal text-[1.3rem] p-5 bg-sky-700 bg-opacity-90 rounded-lg  shadow-[0_3px_10px_rgb(0,0,0,0.2)] "
+    return <div className=" font-inter w-[80%] isolate bg-white font-normal text-[1.3rem] p-5  bg-opacity-90 rounded-lg  shadow-[0_3px_10px_rgb(0,0,0,0.2)]  "
 
     >
         <span className="text-[1.3rem] font-[500] text-blue-800 ">#2</span>
-        <p className="font-[500] text-gray-100">
+        <p className="font-[500] text-gray-900">
             “One of the best ways to sabotage content is to not tie it to your goals. Know why you’re creating content.” <span className="font-[600] text-slate-900">– Ellen Gomes, Glint</span>
         </p>
-    </div>
+    </div >
 }
