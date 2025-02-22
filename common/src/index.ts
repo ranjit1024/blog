22923a8af19d9cf5395e648bf0b536a5aa3cb2f7
ranjit1024 <ranjitdas2048@gmail.com>
2025-02-22 08:00:41 +0530

    added zod validation

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,2 +1,28 @@
-const pi: number = 3.14;
-console.log(pi)
+import z from "zod";
+
+const signupInput = z.object({
+    firstname: z.string(),
+    lastname: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+})
+
+const singinInput = z.object({
+    email: z.string().email(),
+    password: z.string().min(6)
+})
+
+const blogInput = z.object({
+    title: z.string(),
+    content: z.string()
+})
+
+const updateBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+    id: z.number()
+})
+export type SignupInput = z.infer<typeof signupInput>
+export type SigninInput = z.infer<typeof singinInput>
+export type BlogInput = z.infer<typeof blogInput>
+export type UpdateInput = z.infer<typeof updateBlogInput>
